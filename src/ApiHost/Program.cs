12a3b38bb9ff77800ba891412d6066dcb8583088
vinaymanala <vinay.manala@outlook.com>
2025-08-25 23:12:02 +0530

    [FEAT](Configuration) - Adds swagger configuration references

--- src/ApiHost/Program.cs
+++ src/ApiHost/Program.cs
@@ -1,4 +1,5 @@
 using Backend.Core.Config;
+using Backend.ApiHost.Setup;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,26 @@ Console.WriteLine("Starting the setup...");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCustomSwagger();
+builder.Services.AddDataStore();
+builder.Services.AddCustomControllers();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+    {
+        options.ShowCommonExtensions();
+        //* 👇 Note that we set up 6 endpoints for the different services.
+        options.SwaggerEndpoint("v1-dashboard/swagger.json", "Dashboard API");
+        options.SwaggerEndpoint("v1-population/swagger.json", "Population API");
+        options.SwaggerEndpoint("v1-health/swagger.json", "Health API");
+        options.SwaggerEndpoint("v1-economy/swagger.json", "Economy API");
+        options.SwaggerEndpoint("v1-employment/swagger.json", "Employment API");
+        options.SwaggerEndpoint("v1-performance/swagger.json", "Performance API");
+    });
+
+app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -20,4 +37,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.Run();
+
 
